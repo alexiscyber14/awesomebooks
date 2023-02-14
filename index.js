18102d833170cbd65231c358211bee979d45c295
Luchy-B <okparabon@gmail.com>
2023-02-14 09:42:30 +0100

    Add JS classes

--- index.js
+++ index.js
@@ -1,58 +1,52 @@
-
-// Get the book collection container and the form
-const bookCollection = document.getElementById("book-collection");
-const bookForm = document.getElementById("book-form");
-
-// Load the books from localStorage
-let books = JSON.parse(localStorage.getItem("books")) || [];
-
-//set book id here
-function getNextId() {
-  let maxId = 0;
-  for (const book of books) {
-    if (book.id > maxId) {
-      maxId = book.id;
+class Book {
+    constructor (id, title, author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
     }
-  }
-  return maxId + 1;
 }
 
-// Render the books
-function renderBooks() {
-  bookCollection.innerHTML = "";
-  for (const book of books) {
-    const bookElement = document.createElement("div");
-    bookElement.innerHTML = `<h4>${book.title}<br> ${book.author}</h4><button class="remove-button">Remove</button> <br> <hr>`;
-    bookElement.querySelector(".remove-button").addEventListener("click", () => {
-      removeBook(book.id);
-    });
-    bookCollection.appendChild(bookElement);
-  }
+class BookCollection {
+    constructor () {
+        this.books = [];
+    }
+    getNextId() {
+        let maxId = 0;
+        for (const book of books) {
+          if (book.id > maxId) {
+            maxId = book.id;
+          }
+        }
+        return maxId + 1;
+      }
+    
+    addBook(title, author) {
+        const id = getNextId();
+          books.push({ id, title, author });
+          localStorage.setItem("books", JSON.stringify(books));
+          renderBooks();
+        }
+
+    removeBook(id) {
+        books = books.filter(book => book.id !== id);
+        localStorage.setItem("books", JSON.stringify(books));
+        renderBooks();
+        }
+    
+    renderBooks() {
+        bookCollection.innerHTML = "";
+        for (const book of books) {
+            const bookElement = document.createElement("div");
+            bookElement.innerHTML = `<h4>${book.title}<br> ${book.author}</h4><button class="remove-button">Remove</button> <br> <hr>`;
+            bookElement.querySelector(".remove-button").addEventListener("click", () => {
+            removeBook(book.id);
+            });
+            bookCollection.appendChild(bookElement);
+        }
+        }
+
+    save(){
+        localStorage.setItem("books", JSON.stringify(books))
+    }
+    
 }
-
-
-// Add a new book
-function addBook(title, author) {
-  const id = getNextId();
-    books.push({ id, title, author });
-    localStorage.setItem("books", JSON.stringify(books));
-    renderBooks();
-  }
-  
-  // Remove specific book
-  function removeBook(id) {
-    books = books.filter(book => book.id !== id);
-    localStorage.setItem("books", JSON.stringify(books));
-    renderBooks();
-  }
-  
-  // Handle the form  on the submit event
-  bookForm.addEventListener("submit", event => {
-    event.preventDefault();
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-    addBook(title, author);
-    bookForm.reset();
-  });
-  
-  renderBooks();
